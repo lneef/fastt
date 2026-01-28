81c69986020f5daaf085b408fea77105cc7292e9
Lukas Neef <lukas.neef@tum.de>
2026-01-28 14:08:01 +0100

    fix: least in window

--- include/transport/window.h
+++ include/transport/window.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "debug.h"
 #include "message.h"
 #include "protocol.h"
 #include "util.h"
@@ -12,7 +13,7 @@
 
 template <uint32_t N> struct window {
   window(uint64_t min_seq)
-      : wd(), front(0), mask(N - 1), least_in_window(min_seq), max_rx(0){}
+      : wd(), front(0), mask(N - 1), least_in_window(min_seq), max_rx(0) {}
 
   uint64_t get_last_acked_packet() const { return least_in_window - 1; }
 
@@ -20,7 +21,7 @@ template <uint32_t N> struct window {
     auto i = index(seq);
     if (beyond_window(seq) || wd[i])
       return false;
-    if (seq > max_rx){
+    if (seq > max_rx) {
       max_rx = seq;
       ts = *msg->get_ts();
     }
@@ -36,17 +37,17 @@ template <uint32_t N> struct window {
 
   bool beyond_window(uint64_t seq) { return seq > least_in_window + mask; }
 
-  template<typename F>
-  uint32_t advance(F &&f) {
+  template <typename F> uint32_t advance(F &&f) {
     assert(mask + 1 == wd.size());
     uint32_t advanced = 0;
     while (wd[front]) {
+      ++least_in_window;
       f(messages[front]);
       wd[front] = false;
       front = (front + 1) & mask;
-      ++least_in_window;
       ++advanced;
     }
+    FASTT_LOG_DEBUG("%lu\n", least_in_window);
     return advanced;
   }
 
@@ -54,9 +55,7 @@ template <uint32_t N> struct window {
     return seq >= least_in_window && seq <= least_in_window + mask;
   }
 
-  uint32_t capacity() const {
-    return least_in_window + mask - max_rx;
-  }
+  uint32_t capacity() const { return least_in_window + mask - max_rx; }
 
   std::size_t __inline index(std::size_t i) {
     assert(i >= least_in_window);
@@ -71,21 +70,24 @@ template <uint32_t N> struct window {
 
   bool has_holes() { return max_rx != least_in_window - 1; }
 
-  uint16_t copy_bitset(protocol::ft_sack_payload *data) {  
+  uint16_t copy_bitset(protocol::ft_sack_payload *data) {
     uint16_t id = 0;
-    std::memset(data, 0, (max_rx - least_in_window + 64) / 64); /* 64 since least_in_window is part of the window */
+    std::memset(data, 0,
+                (max_rx - least_in_window + 64) /
+                    64); /* 64 since least_in_window is part of the window */
     for (auto i = least_in_window; i <= max_rx; ++i, ++id) {
-      auto ind = get_bit_indices_64(id);  
-      data->bit_map[ind.first] |= static_cast<uint64_t>(wd[index(i)]) << ind.second;
+      auto ind = get_bit_indices_64(id);
+      data->bit_map[ind.first] |= static_cast<uint64_t>(wd[index(i)])
+                                  << ind.second;
     }
     return id;
   }
 
   std::size_t last_seq() const { return least_in_window + mask + 1; }
 
-  uint64_t get_ts(){
-      auto now = rte_get_timer_cycles() / get_ticks_us();
-      return now - ts;
+  uint64_t get_ts() {
+    auto now = rte_get_timer_cycles() / get_ticks_us();
+    return now - ts;
   }
 
   std::bitset<N> wd;
