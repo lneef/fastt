ab8ac55daed48f4d93c081283c9f6e7b056c5637
Lukas Neef <lukas.neef@tum.de>
2026-02-05 12:09:27 +0100

    remove pop

--- include/uring/iface.h
+++ include/uring/iface.h
@@ -63,7 +63,7 @@ struct buffer_pool {
     size = (elemsize * n + psize + sizeof(header) - 1) & (psize - 1);
     base = static_cast<uint8_t *>(mmap(nullptr, size,
                                        PROT_READ | PROT_WRITE,
-                                       MAP_PRIVATE | MAP_POPULATE | MAP_ANON, -1, 0));
+                                       MAP_PRIVATE | MAP_ANON, -1, 0));
     for (auto i = 0u; i < n; ++i) {
       new (base + i * elemsize) header{(i + 1) * elemsize};
     }
