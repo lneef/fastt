ebdcc79287649ac99c091774f54d9b9b1379b7a1
Lukas Neef <lukas.neef@tum.de>
2026-02-05 18:10:08 +0100

    increase ring size

--- include/uring/iface.h
+++ include/uring/iface.h
@@ -15,7 +15,7 @@
 #include <vector>
 
 namespace uring {
-static constexpr int kQueueDepth = 128;
+static constexpr int kQueueDepth = 256;
 static constexpr int kNumBuffer = kQueueDepth * 16;
 static constexpr int kBufShift = 12;
 static constexpr unsigned kCQEntries = kQueueDepth * 8;
