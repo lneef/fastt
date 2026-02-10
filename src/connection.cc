57ed6d4eae0021cc754a537bedf5ba9215770e8a
Lukas Neef <lukas.neef@tum.de>
2026-02-10 12:47:19 +0100

    process pkt

--- src/connection.cc
+++ src/connection.cc
@@ -1,10 +1,8 @@
 #include "connection.h"
 #include "debug.h"
 #include "message.h"
-#include "protocol.h"
 
 #include <cassert>
-#include <cstdlib>
 #include <rte_branch_prediction.h>
 #include <rte_ethdev.h>
 #include <rte_ether.h>
@@ -13,19 +11,7 @@
 #include <rte_memcpy.h>
 
 void connection::process_pkt(rte_mbuf *pkt) {
-  for(auto* msg = pkt; msg; ){
-      auto* hdr = static_cast<message*>(pkt)->data<protocol::ft_header>();
-      if(unlikely(hdr->len > RTE_ETHER_MAX_LEN)){
-          assert(0 && "Jumbo Frames not supported");
-          std::abort();
-      }
-      auto *tpkt = msg;
-      msg = msg->next;
-      tpkt->next = nullptr;
-      tpkt->nb_segs = 1;
-      tpkt->pkt_len = tpkt->data_len;
-      transport_impl->process_pkt(msg_fragment(static_cast<message*>(tpkt)));
-  }   
+    transport_impl->process_pkt(msg_fragment(static_cast<message*>(pkt)));   
 } 
 
 void connection::acknowledge_all(){
