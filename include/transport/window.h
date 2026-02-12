dfcfb261b5dd769ca3cc5bc548eb7edeb81e4b9f
Lukas Neef <lukas.neef@tum.de>
2026-02-12 17:48:27 +0100

    shrink headroom

--- include/transport/window.h
+++ include/transport/window.h
@@ -45,7 +45,7 @@ template <uint32_t width> struct window {
     uint32_t advanced = 0;
     while (wd[front]) {
       ++least_in_window;  
-      mwd[front]->move_headroom<protocol::ft_header>();
+      mwd[front]->shrink_headroom(sizeof(protocol::ft_header));
       f(mwd[front]);
       wd[front] = false;
       mwd[front] = nullptr;
