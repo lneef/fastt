0fb18db37c13e40cc49055d78801bd830454174e
Lukas Neef <lukas.neef@tum.de>
2026-02-23 18:32:36 +0100

    noinline

--- client_main.cc
+++ client_main.cc
@@ -146,7 +146,8 @@ static void run(lcore_function_t *f, void *args) {
   rte_eal_mp_remote_launch(f, args, CALL_MAIN);
   rte_eal_mp_wait_lcore();
 }
-int run(netconfig &conf) {
+
+[[gnu::noinline]] int run(netconfig &conf) {
   if (fastt::init())
     return -1;
 
