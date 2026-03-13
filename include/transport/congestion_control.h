47f4f4a0fd5f505cdd35680ac35e64d6e9a76a22
Lukas Neef <lukas.neef@tum.de>
2026-03-13 17:28:30 +0100

    add wnd

--- include/transport/congestion_control.h
+++ include/transport/congestion_control.h
@@ -4,6 +4,7 @@
 #include "util.h"
 #include <algorithm>
 #include <cstdint>
+#include <generic/rte_cycles.h>
 #include <hdr/hdr_histogram.h>
 
 /*
@@ -13,6 +14,109 @@
  * additionly it is only supposed to avoid completely downing the receiver in
  * case of medium scale incasts
  */
+
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * win_minmax.h: windowed min/max tracker by Kathleen Nichols.
+ *
+ */
+namespace linux_tcp{
+/* A single data point for our parameterized min-max tracker */
+struct minmax_sample {
+	uint64_t	t;	/* time measurement was taken */
+	uint64_t	v;	/* value measured */
+};
+
+/* State for the parameterized min-max tracker */
+struct minmax {
+	struct minmax_sample s[3];
+};
+
+static inline uint64_t minmax_get(const struct minmax *m)
+{
+	return m->s[0].v;
+}
+
+static inline uint64_t minmax_reset(struct minmax *m, uint64_t t, uint64_t meas)
+{
+	struct minmax_sample val = { .t = t, .v = meas };
+
+	m->s[2] = m->s[1] = m->s[0] = val;
+	return m->s[0].v;
+}
+
+
+static uint64_t minmax_subwin_update(struct minmax *m, uint64_t win,
+				const struct minmax_sample *val)
+{
+	uint64_t dt = val->t - m->s[0].t;
+
+	if (unlikely(dt > win)) {
+		/*
+		 * Passed entire window without a new val so make 2nd
+		 * choice the new val & 3rd choice the new 2nd choice.
+		 * we may have to iterate this since our 2nd choice
+		 * may also be outside the window (we checked on entry
+		 * that the third choice was in the window).
+		 */
+		m->s[0] = m->s[1];
+		m->s[1] = m->s[2];
+		m->s[2] = *val;
+		if (unlikely(val->t - m->s[0].t > win)) {
+			m->s[0] = m->s[1];
+			m->s[1] = m->s[2];
+			m->s[2] = *val;
+		}
+	} else if (unlikely(m->s[1].t == m->s[0].t) && dt > win/4) {
+		/*
+		 * We've passed a quarter of the window without a new val
+		 * so take a 2nd choice from the 2nd quarter of the window.
+		 */
+		m->s[2] = m->s[1] = *val;
+	} else if (unlikely(m->s[2].t == m->s[1].t) && dt > win/2) {
+		/*
+		 * We've passed half the window without finding a new val
+		 * so take a 3rd choice from the last half of the window
+		 */
+		m->s[2] = *val;
+	}
+	return m->s[0].v;
+}
+
+/* Check if new measurement updates the 1st, 2nd or 3rd choice max. */
+inline uint64_t minmax_running_max(struct minmax *m, uint64_t win, uint64_t t, uint64_t meas)
+{
+	struct minmax_sample val = { .t = t, .v = meas };
+
+	if (unlikely(val.v >= m->s[0].v) ||	  /* found new max? */
+	    unlikely(val.t - m->s[2].t > win))	  /* nothing left in window? */
+		return minmax_reset(m, t, meas);  /* forget earlier samples */
+
+	if (unlikely(val.v >= m->s[1].v))
+		m->s[2] = m->s[1] = val;
+	else if (unlikely(val.v >= m->s[2].v))
+		m->s[2] = val;
+
+	return minmax_subwin_update(m, win, &val);
+}
+
+/* Check if new measurement updates the 1st, 2nd or 3rd choice min. */
+inline uint64_t minmax_running_min(struct minmax *m, uint64_t win, uint64_t t, uint64_t meas)
+{
+	struct minmax_sample val = { .t = t, .v = meas };
+
+	if (unlikely(val.v <= m->s[0].v) ||	  /* found new min? */
+	    unlikely(val.t - m->s[2].t > win))	  /* nothing left in window? */
+		return minmax_reset(m, t, meas);  /* forget earlier samples */
+
+	if (unlikely(val.v <= m->s[1].v))
+		m->s[2] = m->s[1] = val;
+	else if (unlikely(val.v <= m->s[2].v))
+		m->s[2] = val;
+
+	return minmax_subwin_update(m, win, &val);
+}
+}
 
 static __inline constexpr float fast_inv_sqrt(float val) {
   int32_t i;
@@ -26,6 +130,7 @@ static __inline constexpr float fast_inv_sqrt(float val) {
   return y;
 }
 
+
 struct swift {
   static constexpr float mss = 1500 - protocol::defs::kftOffset;
   static constexpr float initial_len = 1500 - protocol::defs::kftOffset;
@@ -37,6 +142,7 @@ struct swift {
   float base_target_delay, cwnd_size;
   float pacing = 0;
   hdr_histogram *hist;
+  linux_tcp::minmax mm{};
   const uint64_t min_wd_size;
 
   swift(uint64_t target_delay)
@@ -44,10 +150,12 @@ struct swift {
         base_target_delay(target_delay), cwnd_size(initial_len),
         min_wd_size(initial_len) {
             hdr_init(10, 200, 3, &hist);
+            
         }
 
   void on_ack(uint64_t acked, uint64_t now, uint64_t srtt, uint64_t delay) {
     retransmit_cnt = 0;
+    linux_tcp::minmax_running_min(&mm, get_ticks_us() * 100, now, delay);
     bool can_decrease = now - last_decrease > srtt;
     hdr_record_value(hist, delay / get_ticks_us());
     // Skip hop delay
@@ -55,6 +163,7 @@ struct swift {
         base_target_delay +
         std::max<float>(
             std::min<float>(fast_inv_sqrt(cwnd_size) * 5.4 - 0.48, 5), 0) * get_ticks_us();
+    delay = linux_tcp::minmax_get(&mm);
     if (delay < target_delay) {
       cwnd_size += (ai) / cwnd_size * (acked);
     } else if (can_decrease) {
