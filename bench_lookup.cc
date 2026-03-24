977dbdfa27a0f5771161319da87ece49ed65bb45
Lukas Neef <lukas.neef@tum.de>
2026-03-24 19:15:48 +0100

    bench

--- bench_lookup.cc
+++ bench_lookup.cc
@@ -2,7 +2,9 @@
 #include <chrono>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <random>
+#include <vector>
 
 int main(int argc, char *argv[]) {
   unsigned val_len = 8;
@@ -15,16 +17,22 @@ int main(int argc, char *argv[]) {
   std::mt19937_64 rng{42};
   std::uniform_int_distribution<int64_t> dist(0, bench::kStoreSize - 1);
 
+  std::vector<char> buf(val_len);
+
   // warm up
-  for (int i = 0; i < 1000; ++i)
-    (void)store.find(dist(rng));
+  for (int i = 0; i < 1000; ++i) {
+    auto it = store.find(dist(rng));
+    std::memcpy(buf.data(), it->second.data(), val_len);
+  }
 
   uint64_t count = 0;
   auto start = std::chrono::steady_clock::now();
   auto deadline = start + std::chrono::seconds(1);
 
   while (std::chrono::steady_clock::now() < deadline) {
-    store.find(dist(rng));
+    auto it = store.find(dist(rng));
+    std::memcpy(buf.data(), it->second.data(), val_len);
+    asm volatile("" : : "m"(buf) : "memory");
     ++count;
   }
 
