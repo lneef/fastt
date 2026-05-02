412ede2d7df065578b48286bdc189823d9afc515
Lukas Neef <lukas.neef@tum.de>
2026-05-02 12:28:26 +0200

    adjust rand gen

--- client_main.cc
+++ client_main.cc
@@ -133,7 +133,7 @@ static netconfig parse_cmdline(int argc, char *argv[]) {
 static int lcore_closed_fn(void *arg) {
   std::random_device dev;
   std::mt19937 rng(dev());
-  std::uniform_int_distribution<int64_t> dist(0, keySpace);
+  std::uniform_int_distribution<int64_t> dist(0, keySpace - 1);
   auto *adapter = static_cast<lcore_adapter *>(arg);
   auto me = rte_lcore_index(rte_lcore_id());
   assert(adapter->cifs.size() == adapter->nthreads);
@@ -223,7 +223,7 @@ static int lcore_closed_fn(void *arg) {
 static int lcore_open_fn(void *arg) {
   std::random_device dev;
   std::mt19937 rng(dev());
-  std::uniform_int_distribution<int64_t> dist(0, keySpace);
+  std::uniform_int_distribution<int64_t> dist(0, keySpace - 1);
   auto *adapter = static_cast<lcore_adapter *>(arg);
   auto me = rte_lcore_index(rte_lcore_id());
   auto [port, txq, rxq] = adapter->cifs[me];
